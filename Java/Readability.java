d57e838db8825f5ccee55a0ed8d1814fbc3949a2
Pastor Canayo <pastorelguero@hotmail.com>
2020-08-23 22:53:52 -0600

    added source code

---
+++ Java/Readability.java
@@ -0,0 +1,15 @@
+/**************************************************
+ * Readability
+ * Pastor Canayo
+ * This program is to help about comments and readability
+ *************************************************/
+public class Readability {
+
+	public static void main(String[] args) {
+		// Printing something
+		System.out.println("I love Java");
+		System.out.println("I love Java and I code every day");
+		
+	}// end of main method
+
+}// end of class
