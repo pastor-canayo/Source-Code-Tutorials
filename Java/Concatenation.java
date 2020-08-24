789858d8f4b6fd9079db30c9d5ea12eefa812488
Pastor Canayo <pastorelguero@hotmail.com>
2020-08-23 23:30:21 -0600

    added source code

---
+++ Java/Concatenation.java
@@ -0,0 +1,15 @@
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+		
+		int age = 25;
+		String firstName  = "Pastor";
+		String lastName = "Smith";
+		
+		System.out.println(firstName + " " + lastName + " is " + age + " years old.");
+		System.out.println("He also has a " +  age + " year old car.");
+
+	}
+
+}
