fd302cee9544244150ea7b98f507f585e0b6a1b2
Pastor Canayo <pastorelguero@hotmail.com>
2020-08-23 22:33:36 -0600

    Hello World

---
+++ Java/FirstProgram.java
@@ -0,0 +1,11 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, World");
+		System.out.println("Today is the day that we are starting Java Programming");
+		System.out.println("Tutorials are awesome specially in Java Programming");
+	}
+
+}
