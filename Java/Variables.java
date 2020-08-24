db1ea6918e1695f671c29b693634e152d0c62b63
Pastor Canayo <pastorelguero@hotmail.com>
2020-08-23 23:03:43 -0600

    added source code

---
+++ Java/Variables.java
@@ -0,0 +1,23 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int age = 25;
+		double studentGPA = 3.9;
+		char studentFirstInitials = 'P';
+		char studentLastInitials = 'C';
+		String firstName = "Pastor";
+		String lastName = "Smith";
+		
+		//printing info from variables
+		System.out.println("age");
+		System.out.println("studentGPA");
+		System.out.println("studentFirstInitials");
+		System.out.println("studentLastInitials");
+		System.out.println("firstName");
+		System.out.println("lastName");
+
+	}//end of main
+
+}// end of class
