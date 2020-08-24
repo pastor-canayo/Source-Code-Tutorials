2cd3c6b05da58aec9c0655bae61f1750145c3f99
Pastor Canayo <pastorelguero@hotmail.com>
2020-08-23 22:45:20 -0600

    added source code

--- Java/FirstProgram.java
+++ Java/FirstProgram.java
@@ -3,7 +3,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, World");
+		System.out.println("Hello, World!");
 		System.out.println("Today is the day that we are starting Java Programming");
 		System.out.println("Tutorials are awesome specially in Java Programming");
 	}
