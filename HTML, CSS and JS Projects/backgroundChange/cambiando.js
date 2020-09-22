1184df7403bf691d6abe4f7d9e039d27663f148b
Pastor Canayo <pastorelguero@hotmail.com>
2020-09-22 03:24:05 -0600

    Add files via upload
    Added source code

---
+++ HTML, CSS and JS Projects/backgroundChange/cambiando.js
@@ -0,0 +1,16 @@
+function changeBackground(){
+  document.getElementById("wrapper").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('images/birds.jpg')";
+  document.getElementById("wrapper").style.backgroundSize = "cover";
+}
+function changeBackground2(){
+  document.getElementById("wrapper").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('images/kidboss.jpg')";
+  document.getElementById("wrapper").style.backgroundSize = "cover";
+}
+function changeBackground3(){
+  document.getElementById("wrapper").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('images/ideas.jpg')";
+  document.getElementById("wrapper").style.backgroundSize = "cover";
+}
+function changeBackground4(){
+  document.getElementById("wrapper").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('images/rocket.jpg')";
+  document.getElementById("wrapper").style.backgroundSize = "cover";
+}
